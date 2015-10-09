257297d5787bc13432a07e9f8f5b66051d898e06
Ronnie Overby <roverby@coretechs.net>
2015-10-09 09:16:24 -0400

    finished "GenEvent" step

--- src/kv/lib/kv/registry.ex
+++ src/kv/lib/kv/registry.ex
@@ -1,74 +1,71 @@
 defmodule KV.Registry do
-	use GenServer
+  use GenServer
 
-	## Client API
+  ## Client API
 
-	@doc """
-	Starts the registry.
-	"""
-	def start_link(opts \\ []) do
-		GenServer.start_link(__MODULE__, :ok, opts)
-	end
+  @doc """
+  Starts the registry.
+  """
+  def start_link(event_manager, opts \\ []) do
+    # 1. start_link now expects the event manager as argument
+    GenServer.start_link(__MODULE__, event_manager, opts)
+  end
 
-	@doc """
-	Looks up the bucket pid for `name` stored in `server`.
+  @doc """
+  Looks up the bucket pid for `name` stored in `server`.
 
-	Returns `{:ok, pid}` if the bucket exists, `:error` otherwise.
-	"""
-	def lookup(server, name) do
-		GenServer.call(server, {:lookup, name})
-	end
+  Returns `{:ok, pid}` in case a bucket exists, `:error` otherwise.
+  """
+  def lookup(server, name) do
+    GenServer.call(server, {:lookup, name})
+  end
 
-	@doc """	
-	Ensures there is a bucket associated to the given `name` in `server`.
-	"""
-	def create(server, name) do
-		GenServer.cast(server, {:create, name})
-	end
+  @doc """
+  Ensures there is a bucket associated with the given `name` in `server`.
+  """
+  def create(server, name) do
+    GenServer.cast(server, {:create, name})
+  end
 
-	@doc """
-	Stops the registry.
-	"""
-	def stop(server) do
-		GenServer.call(server, :stop)
-	end
+  ## Server callbacks
 
-	## Server Callbacks
+  def init(events) do
+    # 2. The init callback now receives the event manager.
+    #    We have also changed the manager state from a tuple
+    #    to a map, allowing us to add new fields in the future
+    #    without needing to rewrite all callbacks.
+    names = HashDict.new
+    refs  = HashDict.new
+    {:ok, %{names: names, refs: refs, events: events}}
+  end
 
-	def init(:ok) do
-		names = HashDict.new
-		refs = HashDict.new
-		{:ok, {names, refs}}
-	end
+  def handle_call({:lookup, name}, _from, state) do
+    {:reply, HashDict.fetch(state.names, name), state}
+  end
 
-	def handle_call({:lookup, name}, _from, {names, _} = state) do
-		{:reply, HashDict.fetch(names, name), state}
-	end
+  def handle_cast({:create, name}, state) do
+    if HashDict.get(state.names, name) do
+      {:noreply, state}
+    else
+      {:ok, pid} = KV.Bucket.start_link()
+      ref = Process.monitor(pid)
+      refs = HashDict.put(state.refs, ref, name)
+      names = HashDict.put(state.names, name, pid)
+      # 3. Push a notification to the event manager on create
+      GenEvent.sync_notify(state.events, {:create, name, pid})
+      {:noreply, %{state | names: names, refs: refs}}
+    end
+  end
 
-	def handle_call(:stop, _from, state) do
-		{:stop, :normal, :ok, state}
-	end
+  def handle_info({:DOWN, ref, :process, pid, _reason}, state) do
+    {name, refs} = HashDict.pop(state.refs, ref)
+    names = HashDict.delete(state.names, name)
+    # 4. Push a notification to the event manager on exit
+    GenEvent.sync_notify(state.events, {:exit, name, pid})
+    {:noreply, %{state | names: names, refs: refs}}
+  end
 
-	def handle_cast({:create, name}, {names, refs}) do
-		if HashDict.has_key?(names, name) do
-			{:noreply, {names, refs}}
-		else
-			{:ok, pid} = KV.Bucket.start_link()
-			ref = Process.monitor(pid)
-			refs = HashDict.put(refs, ref, name)
-			names = HashDict.put(names, name, pid)
-			{:noreply, {names, refs}}
-		end
-	end
-
-	def handle_info({:DOWN, ref, :process, _pid, _reason}, {names, refs}) do
-		{name, refs} = HashDict.pop(refs, ref)
-		names = HashDict.delete(names, name)
-		{:noreply, {names, refs}}
-	end
-
-	def handle_info(_msg, state) do
-		{:noreply, state}
-	end
-
+  def handle_info(_msg, state) do
+    {:noreply, state}
+  end
 end
